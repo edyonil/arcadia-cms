7485bfb6efad90d189e968b64d587da7a1c30eaa
Claudio Ludovico <ludovico1990@hotmail.it>
2013-07-16 18:37:50 +0200

    [Update] change the default Table.

--- app/database/migrations/relations.sql
+++ app/database/migrations/relations.sql
@@ -4,10 +4,10 @@
  * I Know that this is not the best way, I will fix the problem I swear
  **/
 
-ALTER TABLE  `users` ADD CONSTRAINT  `Users_BelongsTo_Role` FOREIGN KEY (  `role_id` ) REFERENCES  `boilerplate`.`roles` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE ;
+ALTER TABLE  `users` ADD CONSTRAINT  `Users_BelongsTo_Role` FOREIGN KEY (  `role_id` ) REFERENCES  `arcadia`.`roles` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE ;
 
-ALTER TABLE  `posts` ADD CONSTRAINT  `Posts_BelongsTo_User` FOREIGN KEY (  `user_id` ) REFERENCES  `boilerplate`.`users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ;
+ALTER TABLE  `posts` ADD CONSTRAINT  `Posts_BelongsTo_User` FOREIGN KEY (  `user_id` ) REFERENCES  `arcadia`.`users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ;
 
-ALTER TABLE  `comments` ADD CONSTRAINT  `Comments_BelongsTo_Post` FOREIGN KEY (  `post_id` ) REFERENCES  `boilerplate`.`posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ;
+ALTER TABLE  `comments` ADD CONSTRAINT  `Comments_BelongsTo_Post` FOREIGN KEY (  `post_id` ) REFERENCES  `arcadia`.`posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ;
 
-ALTER TABLE  `comments` ADD CONSTRAINT  `Comments_BelongsTo_User` FOREIGN KEY (  `user_id` ) REFERENCES  `boilerplate`.`users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ;
+ALTER TABLE  `comments` ADD CONSTRAINT  `Comments_BelongsTo_User` FOREIGN KEY (  `user_id` ) REFERENCES  `arcadia`.`users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE ;
