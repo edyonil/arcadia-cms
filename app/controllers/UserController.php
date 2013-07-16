afb636d01b94a44e7fa58f7ead3a6bcb919a7740
Claudio Ludovico <ludovico1990@hotmail.it>
2013-07-16 12:11:28 +0200

    [NEW] Restful admin controller

---
+++ app/controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+class UserController extends \BaseController {
+
+	protected $layout = 'adminMaster';
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$this->layout->content = View::make('private.userViews.index');
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+	}
+
+
+	public function showDestroy($id)
+	{
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+	}
+}
