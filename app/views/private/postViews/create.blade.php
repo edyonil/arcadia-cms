4ede89bd50e2ff1b004e8d698182d5af7a85f63f
Claudio Ludovico <ludovico1990@hotmail.it>
2013-07-16 18:38:04 +0200

    [Update] Now you can create new post

--- app/views/private/postViews/create.blade.php
+++ app/views/private/postViews/create.blade.php
@@ -5,6 +5,37 @@
 		<dd><a href="{{ URL::action('PostController@index') }}">All Posts</a></dd>
 		<dd class="active"><a href="{{ URL::action('PostController@create') }}">Create New Post</a></dd>
 	</dl>
-	<h1>Coming soon...</h1>
+	<form action="{{ URL::action('PostController@store') }}" method="post">
+		<fieldset>
+            <legend>Creating a brand new Post</legend>
+			<div class="row">
+				<div class="large-12 columns">
+					@if( $errors->has('pTitle') )
+						<label for="pTitle" class="error">Title</label>
+						<input type="text" name="pTitle" class="error" x-webkit-speech required placeholder="{{ Input::old('pTitle') }}" autocomplete="off">
+						<small class="error">{{ $errors->first('pTitle', ':message') }}</small>
+					@else
+						<label for="pTitle">Title</label>
+						<input type="text" name="pTitle" x-webkit-speech required placeholder="Post Title here">
+					@endif
+				</div>
+			</div>
+			<div class="row">
+				<div class="large-12 columns">
+					@if( $errors->has('pText') )
+						<label for="pText" class="error">Title</label>
+						<textarea name="pText" required placeholder="Put your Text here" class="error" style="margin: 0px 0px 14px;width: 958px;height: 448px;">
+							{{ Input::old('pText') }}
+						</textarea>
+						<small class="error">{{ $errors->first('pText', ':message') }}</small>
+					@else
+						<label for="pText">Text</label>
+						<textarea name="pText" required placeholder="Put your Text here" style="margin: 0px 0px 14px;width: 958px;height: 448px;"></textarea>
+					@endif
+				</div>
+			</div>
+			<button type="submit" class="button success">Save Post</button>
+          </fieldset>
+	</form>
 </div>
 @stop
