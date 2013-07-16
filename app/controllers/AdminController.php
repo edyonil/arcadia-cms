91ab7d4e99749dd7853e46c9292dacc00f876cde
Claudio Ludovico <ludovico1990@hotmail.it>
2013-07-16 12:10:52 +0200

    [Update] move specific method to other controllers

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -8,19 +8,4 @@ class AdminController extends BaseController {
 	{
 		$this->layout->content = View::make('private.index');
 	}
-
-	public function getManagePosts()
-	{
-		$this->layout->content = View::make('private.postViews.index');
-	}
-
-	public function getManageComments()
-	{
-		$this->layout->content = View::make('private.commentViews.index');
-	}
-
-	public function getManageUsers()
-	{
-		$this->layout->content = View::make('private.userViews.index');
-	}
 }
