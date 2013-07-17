fea006d78fcc0181f2fb470cad0f807b146eccc7
Claudio Ludovico <ludovico1990@hotmail.it>
2013-07-17 10:22:14 +0200

    [Update] Discailmer pre-delete

--- app/views/private/postViews/showDestroy.blade.php
+++ app/views/private/postViews/showDestroy.blade.php
@@ -1,3 +1,16 @@
 @section('content')
-
+<div class="row">
+	<dl class="sub-nav">
+		<dt>Options:</dt>
+		<dd><a href="{{ URL::action('PostController@index') }}">All Posts</a></dd>
+		<dd><a href="{{ URL::action('PostController@create') }}">Create New Post</a></dd>
+		<dd class="active"><a href="{{ URL::action('PostController@showDestroy', $post->id) }}">Deleting Post: {{ $post->title }}</a></dd>
+	</dl>
+	<h1>Attention! <small>Are you sure to do this?</small></h1>
+	<form action="{{ URL::action('PostController@destroy', $post->id) }}" method="post">
+		{{ Form::token() }}
+		<input type="hidden" name="_method" value="delete">
+		<input type="submit" class="button alert large-12" value="Yes Delete It!">
+	</form>
+</div>
 @stop
