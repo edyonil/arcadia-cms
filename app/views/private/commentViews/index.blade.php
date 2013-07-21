f1b0dd2b16d36d47c13ef508fe0f974219d227a8
Claudio Ludovico <ludovico1990@hotmail.it>
2013-07-21 10:22:40 +0200

    [Update] Now you can view all the comments

--- app/views/private/commentViews/index.blade.php
+++ app/views/private/commentViews/index.blade.php
@@ -1,3 +1,36 @@
 @section('content')
-<h1>Hello Comments</h1>
+<div class="row">
+	<dl class="sub-nav">
+		<dt>Options:</dt>
+		<dd class="active"><a href="{{ URL::action('CommentController@index') }}">All Comments</a></dd>
+		<dd><a href="{{ URL::action('CommentController@create') }}">Create New Comment</a></dd>
+	</dl>
+	@if( $errors->has('commentActionDone') )
+		<div data-alert class="alert-box success">
+          {{ $errors->first('commentActionDone', ':message') }}
+          <a href="" class="close">×</a>
+        </div>
+	@endif
+	<p>List of all comments</p>
+	<table class="large-12">
+	  <thead>
+	    <tr>
+	      <th>Insert Date</th>
+	      <th>Post commented</th>
+	      <th>Comment author</th>
+	      <th colspan="2">Options</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    @foreach($comments as $comment)
+	    <tr>
+	    	<td>{{ $comment->created_at }}</td>
+	    	<td>{{ $comment->comment }}</td>
+	    	<td><a href="#AllCommentsOf">{{ $comment->user->username }}</a></td>
+	    	<td><a href="{{ URL::action('CommentController@edit', $comment->id) }}">Edit</a></td>
+	    </tr>
+	    @endforeach
+	  </tbody>
+	</table>
+</div>
 @stop
